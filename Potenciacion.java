1c4e43c96f3eb2c320627e19f363e29fc60433cf
Jaime Jaramillo <jaime.jaramillop@uniagustiniana.edu.co>
2023-02-25 13:53:53 -0500

    Agregar validacion base y exponente negativos

--- Potenciacion.java
+++ Potenciacion.java
@@ -9,37 +9,6 @@ public class Potenciacion {
 		this.exponente = exponente;
 	}
 
-	public float getPotenciacion() {
-
-		if(exponente < 0) {
-			exponente = -exponente;
-
-			if(base < 0) {
-				base = -base;
-				if(exponente % 2 != 0)
-					resultado = -calcular(base, exponente);
-				else 
-					resultado = calcular(base, exponente);
-			}else
-				resultado = calcular(base, exponente);
-
-			resultado = 1/resultado; 
-
-		}else {
-
-			if(base < 0) {
-				base = -base;
-				if(exponente % 2 != 0)
-					resultado = -calcular(base, exponente);
-				else
-					resultado = calcular(base, exponente);
-			}else 
-				resultado = calcular(base, exponente);
-		}
-
-		return resultado;
-	}
-
 	private int multiplicar(int base, int exponente) {
 		if(exponente == 1) {
 			return base;
@@ -59,6 +28,28 @@ public class Potenciacion {
 		}
 			
 		return  multiplicar(base,calcular(base,exponente - 1));
+	}
+
+	private float verificarBaseNegativa(int base, int exponente) {
+		if(base < 0) {
+			base = -base;
+			if(exponente % 2 != 0)
+				return -calcular(base, exponente);
+			else
+				return calcular(base, exponente);
+		}
+		
+		return calcular(base, exponente);
+	}
+
+	public float getPotenciacion() {
+		if(exponente < 0) {
+			exponente = -exponente;
+			resultado = verificarBaseNegativa(base, exponente);
+			return  1/resultado; 
+		}
+		
+		return verificarBaseNegativa(base, exponente);
 	}
 
 }
--- Principal.java
+++ Principal.java
@@ -1,8 +1,78 @@
 public class Principal {
 
 	public static void main(String [] args) {
-		Potenciacion p = new Potenciacion(-3,-3);
-		System.out.println(p.getPotenciacion());
-	}
+		Potenciacion p1 = new Potenciacion(4,3);
+		System.out.println(p1.getPotenciacion());
 
+		Potenciacion p2 = new Potenciacion(4,4);
+		System.out.println(p2.getPotenciacion());
+
+		Potenciacion p3 = new Potenciacion(-4,3);
+		System.out.println(p3.getPotenciacion());
+
+		Potenciacion p4 = new Potenciacion(-4,4);
+		System.out.println(p4.getPotenciacion());
+
+		Potenciacion p5 = new Potenciacion(4,-3);
+		System.out.println(p5.getPotenciacion());
+
+		Potenciacion p6 = new Potenciacion(4,-4);
+		System.out.println(p6.getPotenciacion());
+
+		Potenciacion p7 = new Potenciacion(-4,-3);
+		System.out.println(p7.getPotenciacion());
+
+		Potenciacion p8 = new Potenciacion(-4,-4);
+		System.out.println(p8.getPotenciacion() + "\n");
+
+
+		Potenciacion p10 = new Potenciacion(5,3);
+		System.out.println(p10.getPotenciacion());
+
+		Potenciacion p11 = new Potenciacion(5,4);
+		System.out.println(p11.getPotenciacion());
+
+		Potenciacion p12 = new Potenciacion(-5,3);
+		System.out.println(p12.getPotenciacion());
+
+		Potenciacion p13 = new Potenciacion(-5,4);
+		System.out.println(p13.getPotenciacion());
+
+		Potenciacion p14 = new Potenciacion(5,-3);
+		System.out.println(p14.getPotenciacion());
+
+		Potenciacion p15 = new Potenciacion(5,-4);
+		System.out.println(p15.getPotenciacion());
+
+		Potenciacion p16 = new Potenciacion(-5,-3);
+		System.out.println(p16.getPotenciacion());
+
+		Potenciacion p17 = new Potenciacion(-5,-4);
+		System.out.println(p17.getPotenciacion());
+	}
+	/*
+		4^3 = 64
+		4^4 = 256
+
+	     -4^3 = -64
+	     -4^4 = 256
+
+		4^-3 = 0.015625
+		4^-4 = 0.00390625
+
+	     -4^-3 = -0.015625
+	     -4^-4 = 0.00390625
+
+		5^3 = 125
+		5^4 = 625
+
+	     -5^3 = -125
+	     -5^4 = 625
+
+		5^-3 = 0.008
+		5^-4 = 0.0016
+
+	     -5^-3 = -0.008
+	     -5^-4 = 0.0016
+	*/
 }
